d0082c9a85e821384df3b1a292f64319417260fa
Igor Rocha <igor.rocha.7276@gmail.com>
2026-06-27 19:24:49 -0300

    - Ajustando o tamanho dos obstaculos, sua  frequência de spawn e suas posições

--- code/obstacle.py
+++ code/obstacle.py
@@ -5,13 +5,15 @@ from code.config import WIN_WIDTH, WIN_HEIGHT
 
 class Obstacle:
     def __init__(self):
-        self.size = random.randint(60, 90)
+        self.size = random.randint(60, 120)
         self.image = pygame.transform.scale(pygame.image.load('./assets/imagens/ast.png').convert_alpha(),
                                             (self.size, self.size))
-        self.rect = self.image.get_rect(x=random.randint(0, WIN_WIDTH - self.size), y=-100).inflate(-20, -20)
 
-        self.is_destructible = random.random() < 0.25
-        self.speed = random.randint(3, 6)
+        self.rect = self.image.get_rect(x=random.randint(0, WIN_WIDTH - self.size),
+                                        y=random.randint(-500, -50)).inflate(-30, -30)
+
+        self.is_destructible = random.random() < 0.15
+        self.speed = random.randint(3, 7)
         self.blink_timer = 0
         self.visible = True
 
@@ -26,7 +28,8 @@ class Obstacle:
 
         if self.rect.top > WIN_HEIGHT:
             self.rect.x = random.randint(0, WIN_WIDTH - self.size)
-            self.rect.y = random.randint(-150, -50)
+            self.rect.y = random.randint(-600, -100)
+            self.is_destructible = random.random() < 0.15
             return True
         return False
 
